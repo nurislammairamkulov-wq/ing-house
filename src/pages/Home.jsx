9366d0d7f37e5c33ff1fdfcf798f55926d2b11c1
Nurislam <nurislammairamkulov@gmail.com>
2026-01-08 13:09:11 +0600

    swdc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import Header from "../components/header/header";
+import Card from "../components/card/card";
 
 function Home() {
   return (
     <>
       <Header />
+      <Card />
+      <ing-house />
     </>
   );
 }
