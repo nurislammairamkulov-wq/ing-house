784a97b79d519bb4dd59588fbc5f546e0b022c59
Nurislam <nurislammairamkulov@gmail.com>
2026-01-08 11:10:19 +0600

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,17 @@
-import Image from "./ink.House.svg";
+import Image1 from "./ink.House.svg";
+import Image2 from "./cart.svg";
+import "./header.css";
 
 function Header() {
   return (
     <header>
-      <img src={Image} alt="" />
+      <img src={Image1} alt="" />
+      <div>
+        <a href="">Репродукции</a>
+        <a href="">Новинки</a>
+        <a href="">О нас</a>
+      </div>
+      <img src={Image2} alt="" />
     </header>
   );
 }
